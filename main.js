e58f6cf4caa14a361cb69c798731cf2df138080e
Collin Perkins <Collin Perkins>
2016-09-20 09:10:03 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  var qrObject;
+  var qrLot;
+  var image;
+
+  $('#qrScan').change(function() {
+    qrObject = $('#qrScan').val();
+    qrObject = JSON.parse(qrObject);
+    qrLot = qrObject.lot
+
+    image = JsBarcode("#barcode", qrLot);
+    console.log(image);
+    image;
+    image = '<body><div id="barcode">' + image + '</div></body>';
+  })
+
+  $('.qrPrint').click(function () {
+
+    var w = window.open();
+    w.document.write(
+    `<html><head><body><div>${$("#eanBarcode").html()}</div></body></html>`
+    );
+    w.print();
+    w.close();
+  })
+
+})
